44d361de5433c5225a1957fd9d6b377077327cc7
salm0027 <mustapha.salmi@etudiant.univ-reims.fr>
2023-02-17 09:55:00 +0100

    Update src/Controller/EvenementController.php

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -26,17 +26,13 @@ class EvenementController extends AbstractController
     $date_end = $request->get('date_end');
     
     if($lib_groupe){
-      // $evenements = $evenementRepository->findByIntervaleAndGroupe($date_start, $date_end, $lib_groupe, EntityManager::class);
-
       $entityManager = $this->getDoctrine()->getManager();
 
-      // Récupérer le repository (la classe qui gère les requêtes) pour l'entité Evenement
       $evenementRepository = $entityManager->getRepository(Evenement::class);
 
-      // Récupérer le repository pour l'entité Groupe
       $groupeRepository = $entityManager->getRepository(Groupe::class);
 
-      // Récupérer tous les événements associés à un groupe avec un libellé spécifique
+
       $evenements = $evenementRepository->createQueryBuilder('e')
           ->join('e.concerne', 'g')
           ->where('g.lib_groupe LIKE :libelle')
@@ -52,9 +48,9 @@ class EvenementController extends AbstractController
       $evenements = $evenementRepository->findByIntervale($date_start, $date_end);
     }
 
-    // if(!$evenements){
-    //   return $this->json(['message' => 'Aucun evenement trouvé'], 404);
-    // }
+   if(!$evenements){
+       return $this->json(['message' => 'Aucun evenement trouvé'], 404);
+     }
 
     return $this->json([$evenements], 200);
   }
